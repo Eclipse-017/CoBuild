af775d29f4945115e9b585fd4d50c7c254b2512c
Chioma Okpor <estee.ojumah@gmail.com>
2026-06-02 16:04:49 +0100

    I just finished working on the Image component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 // import { useState } from 'react'
 import './App.css'
+import ImageSection from './components/imageSection/ImageSection'
+import './components/imageSection/imageSection.css'
 
 function App() {
 
 
   return (
     <>
-      
+      <ImageSection />
     </>
   )
 }
