e4c399f9fe38c31beafc04f0c6264795f8b7b5ea
Eclipse-017 <ajibadeabdulsalam42@gmail.com>
2026-06-04 12:02:43 +0100

    Fix: Remove unused imports and components from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import './App.css'
 import VideoSection from './components/VideoSection/VideoSection'
-import FormSection from './components/FormSection'
 
 function App(){
   return( 
     <>
       <VideoSection src='/public/video/video.mp4'/>
-      <FormSection></FormSection>
     </>
   )
 }
