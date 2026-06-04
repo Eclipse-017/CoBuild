4528fe4e1af8f01e2c9c90767d09c31028088809
silver-orioha <silverstonegirl@gmail.com>
2026-06-04 08:02:58 +0100

    Inserted table section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import VideoSection from './components/VideoSection/VideoSection'
+import TableSection from './components/TableSection'
 import FormSection from './components/FormSection'
 
 function App(){
   return( 
     <>
       <VideoSection src='/public/video/video.mp4'/>
+      <TableSection/>
       <FormSection></FormSection>
     </>
   )
