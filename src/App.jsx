d0b6edcc8135929e01f9f935308048f57c7bee0c
Richard Okafor <javadevrekahdo@gmail.com>
2026-06-05 16:59:23 +0100

    save progress

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,11 @@ import './styles/reset.css'
 import './styles/utilities.css'
 import './styles/queries.css'
 
-import FormSection from './components/FormSection/FormSection'
 import VideoSection from './components/VideoSection/VideoSection'
 
 function App(){
   return( 
     <>
-      <FormSection />
       <VideoSection src='/public/video/video.mp4'/>
     </>
   )
